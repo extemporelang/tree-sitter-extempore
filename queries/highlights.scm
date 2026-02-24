34c03d1d37ef8be8cd36ff7b043e6b77a0129f62
Ben Swift <ben@benswift.me>
2026-02-24 11:14:56 +1100

    fix highlight priority ordering and add compound type tests
    Reorder highlights.scm so the catch-all @function.call pattern comes
    first (lowest priority) and specific bind-*/define patterns come last
    (highest priority). Previously the catch-all was overriding the more
    specific patterns, preventing bind-* keywords and their names from
    being highlighted correctly.
    
    Add 14 test corpus entries for compound type patterns: arrays of
    closures, nested arrays in tuples, double-pointer closures, bind-val
    with compound types, recursive named types, and more.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,108 @@
 ; highlights.scm --- Extempore syntax highlighting queries
+;
+; Pattern ordering matters: later patterns take priority over earlier ones.
+; The catch-all @function.call must come first so that more specific patterns
+; (keywords, builtins, bind-* forms) override it.
 
-; xtlang binding forms: highlight the keyword and the name
+; Default: first symbol in a list is a function call (lowest priority)
+(list
+  .
+  (symbol) @function.call)
+
+; xtlang type expressions
+(xtlang_type) @type
+(typed_identifier) @variable
+(generic_identifier) @type
+
+; Literals
+(number) @number
+(string) @string
+(escape_sequence) @string.escape
+(boolean) @constant.builtin
+(character) @constant.builtin
+
+; Comments
+(comment) @comment
+
+; Punctuation
+["(" ")"] @punctuation.bracket
+["#("] @punctuation.bracket
+
+; Quote forms
+(quote "'") @punctuation.special
+(quasiquote "`") @punctuation.special
+(unquote ",") @punctuation.special
+(unquote_splicing ",@") @punctuation.special
+
+; Operators
+(list
+  .
+  (symbol) @operator
+  (#match? @operator "^(\\+|-|\\*|/|=|<|>|<=|>=|!=|%|<<|>>|bitwise-and|bitwise-or|bitwise-xor|bitwise-not)$"))
+
+; Special xtlang operators
+(list
+  .
+  (symbol) @keyword.operator
+  (#match? @keyword.operator "^(\\$|\\$\\$)$"))
+
+; Builtins: Scheme
+(list
+  .
+  (symbol) @function.builtin
+  (#match? @function.builtin "^(car|cdr|cons|list|append|reverse|length|map|for-each|filter|fold|foldl|foldr|apply|eval|null\\?|pair\\?|number\\?|integer\\?|real\\?|string\\?|symbol\\?|procedure\\?|boolean\\?|char\\?|vector\\?|port\\?|eq\\?|eqv\\?|equal\\?|not|zero\\?|positive\\?|negative\\?|even\\?|odd\\?|abs|min|max|modulo|remainder|quotient|gcd|lcm|floor|ceiling|truncate|round|exact->inexact|inexact->exact|number->string|string->number|string->symbol|symbol->string|string->list|list->string|vector->list|list->vector|char->integer|integer->char|string-ref|string-set!|string-length|string-append|string-copy|substring|string-upcase|string-downcase|make-string|make-vector|vector-ref|vector-set!|vector-length|vector-fill!|assoc|assq|assv|member|memq|memv|sort|display|write|newline|println|print|printf|printout|read|error|format|open-input-file|open-output-file|close-input-port|close-output-port|read-char|write-char|peek-char|char-ready\\?|load|exit|quit|caar|cadr|cdar|cddr|caaar|caadr|cadar|caddr|cdaar|cdadr|cddar|cdddr)$"))
+
+; Builtins: xtlang memory and data
+(list
+  .
+  (symbol) @function.builtin
+  (#match? @function.builtin "^(alloc|salloc|halloc|zalloc|stack-alloc|heap-alloc|zone-alloc|free|tref|tset!|tfill!|aref|aset!|aref-ptr|afill!|pref|pset!|pref-ptr|pfill!|vref|vset!|vfill!|make-array|array-ref|array-set!|null\\?|type-of|size-of|void|now|random|imp_rand)$"))
+
+; Keywords: xtlang control flow and memory
+(list
+  .
+  (symbol) @keyword
+  (#match? @keyword "^(dotimes|doloop|dolet|memzone|callback|cast|convert|bitcast|call-as-xtlang|sync|spawn|async|catch|call/cc|call-with-current-continuation)$"))
+
+; Keywords: Scheme special forms
+(list
+  .
+  (symbol) @keyword
+  (#match? @keyword "^(define|define-macro|macro|lambda|let|let\\*|letrec|letz|if|cond|case|begin|beginz|do|when|unless|set!|and|or|else|delay|receive|quasiquote|unquote|unquote-splicing)$"))
+
+; Keywords: xtlang forms
+(list
+  .
+  (symbol) @keyword
+  (#match? @keyword "^(bind-func|bind-type|bind-data|bind-val|bind-lib|bind-dylib|bind-alias|bind-poly|bind-macro|bind-instrument|bind-sampler|definec|definec:dsp)$"))
+
+; Scheme definition forms
+(list
+  .
+  (symbol) @keyword
+  .
+  (symbol) @variable.definition
+  (#match? @keyword "^define$"))
+
+(list
+  .
+  (symbol) @keyword
+  .
+  (list
+    .
+    (symbol) @function)
+  (#match? @keyword "^define$"))
+
+(list
+  .
+  (symbol) @keyword
+  .
+  (list
+    .
+    (symbol) @function)
+  (#match? @keyword "^define-macro$"))
+
+; xtlang binding forms: highlight the keyword and the name (highest priority)
 (list
   .
   (symbol) @keyword
@@ -70,101 +172,3 @@
   .
   (symbol) @function
   (#match? @keyword "^bind-macro$"))
-
-; Scheme definition forms
-(list
-  .
-  (symbol) @keyword
-  .
-  (symbol) @variable.definition
-  (#match? @keyword "^define$"))
-
-(list
-  .
-  (symbol) @keyword
-  .
-  (list
-    .
-    (symbol) @function)
-  (#match? @keyword "^define$"))
-
-(list
-  .
-  (symbol) @keyword
-  .
-  (list
-    .
-    (symbol) @function)
-  (#match? @keyword "^define-macro$"))
-
-; Keywords: xtlang forms
-(list
-  .
-  (symbol) @keyword
-  (#match? @keyword "^(bind-func|bind-type|bind-data|bind-val|bind-lib|bind-dylib|bind-alias|bind-poly|bind-macro|bind-instrument|bind-sampler|definec|definec:dsp)$"))
-
-; Keywords: Scheme special forms
-(list
-  .
-  (symbol) @keyword
-  (#match? @keyword "^(define|define-macro|macro|lambda|let|let\\*|letrec|letz|if|cond|case|begin|beginz|do|when|unless|set!|and|or|else|delay|receive|quasiquote|unquote|unquote-splicing)$"))
-
-; Keywords: xtlang control flow and memory
-(list
-  .
-  (symbol) @keyword
-  (#match? @keyword "^(dotimes|doloop|dolet|memzone|callback|cast|convert|bitcast|call-as-xtlang|sync|spawn|async|catch|call/cc|call-with-current-continuation)$"))
-
-; Builtins: Scheme
-(list
-  .
-  (symbol) @function.builtin
-  (#match? @function.builtin "^(car|cdr|cons|list|append|reverse|length|map|for-each|filter|fold|foldl|foldr|apply|eval|null\\?|pair\\?|number\\?|integer\\?|real\\?|string\\?|symbol\\?|procedure\\?|boolean\\?|char\\?|vector\\?|port\\?|eq\\?|eqv\\?|equal\\?|not|zero\\?|positive\\?|negative\\?|even\\?|odd\\?|abs|min|max|modulo|remainder|quotient|gcd|lcm|floor|ceiling|truncate|round|exact->inexact|inexact->exact|number->string|string->number|string->symbol|symbol->string|string->list|list->string|vector->list|list->vector|char->integer|integer->char|string-ref|string-set!|string-length|string-append|string-copy|substring|string-upcase|string-downcase|make-string|make-vector|vector-ref|vector-set!|vector-length|vector-fill!|assoc|assq|assv|member|memq|memv|sort|display|write|newline|println|print|printf|printout|read|error|format|open-input-file|open-output-file|close-input-port|close-output-port|read-char|write-char|peek-char|char-ready\\?|load|exit|quit|caar|cadr|cdar|cddr|caaar|caadr|cadar|caddr|cdaar|cdadr|cddar|cdddr)$"))
-
-; Builtins: xtlang memory and data
-(list
-  .
-  (symbol) @function.builtin
-  (#match? @function.builtin "^(alloc|salloc|halloc|zalloc|stack-alloc|heap-alloc|zone-alloc|free|tref|tset!|tfill!|aref|aset!|aref-ptr|afill!|pref|pset!|pref-ptr|pfill!|vref|vset!|vfill!|make-array|array-ref|array-set!|null\\?|type-of|size-of|void|now|random|imp_rand)$"))
-
-; Default: first symbol in a list is a function call
-(list
-  .
-  (symbol) @function.call)
-
-; xtlang type expressions
-(xtlang_type) @type
-(typed_identifier) @variable
-(generic_identifier) @type
-
-; Literals
-(number) @number
-(string) @string
-(escape_sequence) @string.escape
-(boolean) @constant.builtin
-(character) @constant.builtin
-
-; Comments
-(comment) @comment
-
-; Operators
-(list
-  .
-  (symbol) @operator
-  (#match? @operator "^(\\+|-|\\*|/|=|<|>|<=|>=|!=|%|<<|>>|bitwise-and|bitwise-or|bitwise-xor|bitwise-not)$"))
-
-; Special xtlang operators
-(list
-  .
-  (symbol) @keyword.operator
-  (#match? @keyword.operator "^(\\$|\\$\\$)$"))
-
-; Punctuation
-["(" ")"] @punctuation.bracket
-["#("] @punctuation.bracket
-
-; Quote forms
-(quote "'") @punctuation.special
-(quasiquote "`") @punctuation.special
-(unquote ",") @punctuation.special
-(unquote_splicing ",@") @punctuation.special
